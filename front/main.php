2bc8968ca1b66d59f2f56327c35396389f441688
sandy0v0 <be0324ss@gmail.com>
2024-12-30 11:59:53 +0800

    建立主題內容頁籤區塊

--- front/main.php
+++ front/main.php
@@ -1,1 +1,61 @@
-頁籤
+<style>
+.type{
+    border:1px solid #999;
+    padding: 5px 10px;
+    margin-left: -1px;
+    /* margin: 0 1px; */
+}
+
+.types{
+    display: flex;
+    margin-left: 1px;
+}
+
+.texts{
+
+}
+
+.text{
+    width: 95%;
+    min-height:450px;
+    border:1px solid #999;
+    display: none;
+}
+
+.active{
+    display:block;
+}
+
+
+
+</style>
+<div class="types">
+<div class='type'>健康新知</div>
+<div class='type'>菸害防治</div>
+<div class='type'>癌症防治</div>
+<div class='type'>慢性病防治</div>
+</div>
+
+<!-- div.texts>div.text*4 -->
+<div class="texts">
+    <div class="text active">
+1
+    </div>
+    <div class="text">
+2
+    </div>
+    <div class="text">
+3
+    </div>
+    <div class="text">
+4
+    </div>
+</div>
+
+<script>
+    $(".type").on('click',function(){
+        let idx=$(this).index();
+        $(".text").removeClass("active")
+        $(".text").eq(idx).addClass('active')
+    })
+</script>
