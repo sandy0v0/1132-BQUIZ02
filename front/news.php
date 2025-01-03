feae5e1830eaf99cafbaa31f7d9706cab6000ed7
sandy0v0 <be0324ss@gmail.com>
2025-01-03 10:35:02 +0800

    建立最新文章顯示

--- front/news.php
+++ front/news.php
@@ -1,1 +1,45 @@
-最新消息
+<fieldset>
+<legend>目前位置：首頁 > 最新文章區 ></legend>
+
+<!-- table>(tr>th*3)+(tr>th*3) -->
+
+<table style="width:100%;">
+    <tr>
+        <th width="20%">標題</th>
+        <th width="60%">內容</th>
+        <th></th>
+    </tr>
+    <?php
+    $total=$News->count();
+    $div=5;
+    $pages=ceil($total/$div);
+    $now=$_GET['p']??1;
+    $start=($now-1)*$div;
+    $rows=$News->all(['sh'=>1]," Limit $start,$div");
+    foreach($rows as $row):
+    ?>
+    <tr>
+        <th><?=$row['title'];?></th>
+        <th><?=mb_substr($row['news'],0,25);?>...</th>
+        <th></th>
+    </tr>
+    <?php endforeach; ?>
+</table>
+
+<div>
+    <?php
+
+    if(($now-1)>0){
+        echo "<a href='?do=news&p=".($now-1)."'> &lt;</a>";
+    }
+    for($i=1;$i<=$pages;$i++){
+        $size=($i==$now)?"24px":"16px";
+        echo "<a href='?do=news&p=$i' style='font-size:$size'> $i </a>";
+    }
+    if(($now+1)<=$pages){
+        echo "<a href='?do=news&p=".($now+1)."'> &gt;</a>";
+    }
+    ?>
+</div>
+</fieldset>
+
