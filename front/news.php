2377f8ec49404144df001d37b3f5cb81de156ca1
sandy0v0 <be0324ss@gmail.com>
2025-01-03 13:48:05 +0800

    完成點擊顯示完整新聞

--- front/news.php
+++ front/news.php
@@ -1,57 +1,64 @@
+<style>
+    .detail{
+        display:none;
+    }
+</style>
+
 <fieldset>
-<legend>目前位置：首頁 > 最新文章區 ></legend>
+    <legend>目前位置：首頁 > 最新文章區</legend>
 
 <!-- table>(tr>th*3)+(tr>th*3) -->
 
-<table style="width:100%;">
-    <tr>
-        <th width="20%">標題</th>
-        <th width="60%">內容</th>
-        <th></th>
-    </tr>
-    <?php
-    $total=$News->count();
-    $div=5;
-    $pages=ceil($total/$div);
-    $now=$_GET['p']??1;
-    $start=($now-1)*$div;
-    $rows=$News->all(['sh'=>1]," Limit $start,$div");
-    foreach($rows as $row):
-    ?>
-    <tr>
-        <td><?=$row['title'];?></td>
-        <td><?=mb_substr($row['news'],0,25);?>...</td>
-        <td>
+    <table style="width: 100%;">
+        <tr>
+            <th width="20%">標題</th>
+            <th width="60%">內容</th>
+            <th></th>
+        </tr>
         <?php
-            if(isset($_SESSION['user'])){
-                $chk=$Log->count(['news'=>$row['id'],'user'=>$_SESSION['user']]);
-                $like=($chk>0)?"收回讚":"讚";
-                echo "<a href='#' data-id='{$row['id']}' class='like'>$like</a>";
+
+        $total=$News->count();
+        $div=5;
+        $pages=ceil($total/$div);
+        $now=$_GET['p']??1;
+        $start=($now-1)*$div;
+        $rows=$News->all(['sh'=>1]," Limit $start,$div");
+        foreach($rows as $row):
+        ?>
+        <tr>
+            <td class="row-title"><?=$row['title'];?></td>
+            <td>
+                <span class='title'><?=mb_substr($row['news'],0,25);?>...</span>
+                <span class='detail'><?=nl2br($row['news']);?></span>
+            </td>
+            <td>
+                <?php 
+                if(isset($_SESSION['user'])){
+                    $chk=$Log->count(['news'=>$row['id'],'user'=>$_SESSION['user']]);
+                    $like=($chk>0)?"收回讚":"讚";
+                    echo "<a href='#' data-id='{$row['id']}' class='like'>$like</a>";
+                }
+
+                ?>
+            </td>
+        </tr>
+        <?php endforeach; ?>
+    </table>
+    <div>
+        <?php 
+            if(($now-1)>0){
+                echo "<a href='?do=news&p=".($now-1)."'> &lt;</a>";
             }
-
-            ?>
-        </td>
-    </tr>
-    <?php endforeach; ?>
-</table>
-
-<div>
-    <?php
-
-    if(($now-1)>0){
-        echo "<a href='?do=news&p=".($now-1)."'> &lt;</a>";
-    }
-    for($i=1;$i<=$pages;$i++){
-        $size=($i==$now)?"24px":"16px";
-        echo "<a href='?do=news&p=$i' style='font-size:$size'> $i </a>";
-    }
-    if(($now+1)<=$pages){
-        echo "<a href='?do=news&p=".($now+1)."'> &gt;</a>";
-    }
-    ?>
-</div>
+            for($i=1;$i<=$pages;$i++){
+                $size=($i==$now)?"24px":"16px";
+                echo "<a href='?do=news&p=$i' style='font-size:$size'> $i </a>";
+            }
+            if(($now+1)<=$pages){
+                echo "<a href='?do=news&p=".($now+1)."'> &gt;</a>";
+            }
+?>
+    </div>
 </fieldset>
-
 <script>
     $(".like").on("click",function(){
         let id=$(this).data('id');
@@ -69,6 +76,8 @@
             }
         })
     })    
-
+    
+$(".row-title").on("click",function(){
+    $(this).next().children(".title,.detail").toggle();
+})
 </script>
-
